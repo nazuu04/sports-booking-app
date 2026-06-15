812255774dee8cc7adf962adcba32aa345a21704
nazuu04 <nazwanasution04@gmail.com>
2026-06-15 20:50:01 +0700

    debug login

--- admin/login_user.php
+++ admin/login_user.php
@@ -1,4 +1,9 @@
  <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+session_start();
+include "koneksi.php";
 session_start();
 include "koneksi.php";
 if(isset($_SESSION['user_login'])){ header("Location: home.php"); exit; }
