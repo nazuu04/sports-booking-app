de95d9839ad93d67fc3fc04d83cbe06416e207ae
nazuu04 <nazwanasution04@gmail.com>
2026-06-16 19:35:27 +0700

    Perbaiki fitur hapus booking

--- admin/hapus_booking.php
+++ admin/hapus_booking.php
@@ -1,18 +1,12 @@
- <?php
+<?php
 include "koneksi.php";
 
 $id = $_GET['id'];
 
 mysqli_query(
-$conn,
-"DELETE FROM booking WHERE id_booking='$id'"
+    $conn,
+    "DELETE FROM booking WHERE id_booking='$id'"
 );
 
 header("Location: booking.php");
 exit;
-?>
-<a href="hapus_booking.php?id=<?= $d['id_booking']; ?>"
-onclick="return confirm('Yakin ingin menghapus data ini?')">
-Hapus
-</a>
-
