b2ec656f4ec4ebe81434f7b95a471cd9a422f03d
nazuu04 <nazwanasution04@gmail.com>
2026-06-16 13:44:36 +0700

    fix koneksi

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 $conn = mysqli_connect(
     "mysql.railway.internal",
     "root",
@@ -9,9 +6,7 @@ $conn = mysqli_connect(
     "railway"
 );
 
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+if(!$conn){
+    die("Koneksi gagal");
 }
-
-echo "Koneksi berhasil";
 ?>
